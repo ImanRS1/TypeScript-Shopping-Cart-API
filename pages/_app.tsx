eedd9ff5402bc2fae696064aefdbc7ea34f99478
Iman Reza Soltani <iman.soltani@bonniernews.se>
2022-05-09 09:14:54 +0200

    wrapper created, imports added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+
+import Drawer from '@material-ui/core/Drawer'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Grid from '@material-ui/core/Grid'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import Badge from '@material-ui/core/Badge'
+
+import { Wrapper } from '../styles/App.styles'
+
+
+const getProducts = async () => {
+  await (await fetch('https://fakestoreapi.com/products')).json();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
