94e6ff161560985005f983873c5f0e239f558554
Iman Reza Soltani <iman.soltani@bonniernews.se>
2022-05-10 17:25:44 +0200

    CartItemType added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,15 @@ import Badge from '@material-ui/core/Badge'
 
 import { Wrapper } from '../styles/App.styles'
 
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
 
 const getProducts = async () => {
   await (await fetch('https://fakestoreapi.com/products')).json();
