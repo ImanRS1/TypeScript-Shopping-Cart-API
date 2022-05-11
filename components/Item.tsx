75ce720cbae27343fecd0ea2ddd62e07cef80b79
Iman Reza Soltani <iman.soltani@bonniernews.se>
2022-05-11 15:57:19 +0200

    QueryProdiver and code refactored

---
+++ components/Item.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { ButtonGroup } from '@material-ui/core'
+
+const Item = () => {
+  return (
+    <div>Item</div>
+  )
+}
+
+export default Item
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useState } from 'react'
-import { useQuery } from 'react-query'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-import Drawer from '@material-ui/core/Drawer'
-import LinearProgress from '@material-ui/core/LinearProgress'
-import Grid from '@material-ui/core/Grid'
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
-import Badge from '@material-ui/core/Badge'
-
-import { Wrapper } from '../styles/App.styles'
-
-export type CartItemType = {
-  id: number;
-  category: string;
-  description: string;
-  image: string;
-  price: number;
-  title: string;
-  amount: number;
-}
-
-const getProducts = async () => {
-  await (await fetch('https://fakestoreapi.com/products')).json();
-}
+const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={client} contextSharing={true}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
