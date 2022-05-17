128d9fe7d54d6a84e1594f953e6676578d8fd420
Iman Reza Soltani <iman.soltani@bonniernews.se>
2022-05-17 15:08:17 +0200

    cart items styled

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -14,28 +14,33 @@ type Props = {
 
 const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
   <Wrapper>
-    <h3>{item.title}</h3>
-    <div className="information">
-      <p>Price: ${item.price}</p>
-      <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
-      <div className="buttons">
-        <Button
-          size="small"
-          disableElevation
-          variant="contained"
-          onClick={() => removeFromCart(item.id)}
-        >
-          -
-        </Button>
-        <p>{item.amount}</p>
-        <Button
-          size="small"
-          disableElevation
-          variant="contained"
-          onClick={() => addToCart(item)}
-        >
-          +
-        </Button>
+    <div>
+      <h3>{item.title}</h3>
+      <div className="information">
+        <div className="price-info">
+          <p>Price: ${item.price}</p>
+          <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+        </div>
+
+        <div className="buttons">
+          <Button
+            size="small"
+            disableElevation
+            variant="contained"
+            onClick={() => removeFromCart(item.id)}
+          >
+            -
+          </Button>
+          <p>{item.amount}</p>
+          <Button
+            size="small"
+            disableElevation
+            variant="contained"
+            onClick={() => addToCart(item)}
+          >
+            +
+          </Button>
+        </div>
       </div>
     </div>
     <img src={item.image} alt={item.title} />
@@ -44,4 +49,26 @@ const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
 
 export default CartItem;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-family: Arial, Helvetica, sans-serif;
+  border-bottom: 1px solid lightblue;
+  padding-bottom: 20px;
+
+  div {
+    flex: 1;
+  }
+
+  .buttons,
+  .price-info {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  img {
+    max-width: 80px;
+    object-fit: cover;
+    margin-left: 40px;
+  }
+`;
